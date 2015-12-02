d100e116d5bef16b1104d1500743d9b726c61a96
zhaiduo <zhaiduo@gmail.com>
2015-12-02 23:25:31 +0800

    更新设置

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Flask 框架练手
 
+源码基于Flask项目： https://github.com/shekhargulati/instant-flask-web-development-book-app.git
+
 ## 学习目标
 
 * Flask     https://github.com/zhaiduo/flaskScheduleApp
--- flaskScheduleapp.sh
+++ flaskScheduleapp.sh
@@ -4,6 +4,7 @@ git clone https://github.com/shekhargulati/instant-flask-web-development-book-ap
 cd scheduleapp
 virtualenv venv --python=python2.7
 . venv/bin/activate
+pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py create_tables
 python manage.py runserver
