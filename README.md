8164360f3a59a386dcdd454724a06f2b1dcdc60b
lvyadong <lvyadong@hopperclouds.com>
2015-12-02 18:12:53 +0800

    new: init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # flaskScheduleApp
+
+Flask 框架练手
---
+++ flaskScheduleapp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git clone https://github.com/shekhargulati/instant-flask-web-development-book-app.git scheduleapp
+cd scheduleapp
+virtualenv venv --python=python2.7
+. venv/bin/activate
+pip install -r requirements.txt
+python manage.py create_tables
+python manage.py runserver
+
